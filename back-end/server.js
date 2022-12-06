52d7aa888df8cee8161324a7ac76b30bece46f8a
Bulat Constantin <iancu.constantin95@gmail.com>
2022-12-06 09:26:01 +0200

    little changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fetch = require("node-fetch")
 const { default: mongoose } = require('mongoose');
+const Hospitals = require("./models/hospitals.model")
 require('dotenv').config()
 
 // Mongo DB Connections
@@ -23,6 +25,26 @@ app.use(express.json())
 // Routes
 app.get("/", (req,res)=>{
     res.json({message: "GET ROUTE for /"})
+})
+app.get("/hospitals", async(req,res)=>{
+    const response = await fetch("https://api.billingo.hu/v3/partners",{
+        headers: {
+            'X-API-KEY': '2a808996-6f31-11ed-8f89-06ac9760f844',
+            'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+    if(response.ok){
+        Hospitals.insertMany(json.data)
+        .then(function(){
+            console.log("Data inserted")  // Success
+        }).catch(function(error){
+            console.log("error: ",error)      // Failure
+        });
+    }
+    if(!response.ok){
+        res.status(400).json({message:"something went wrong with get data"})
+    }
 })
 
 // Connection
