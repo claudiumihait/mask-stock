60e79fb83b706d2fc64c1af7c51080b0602198e6
claudiumihait <claudium.tudor@gmail.com>
2022-12-07 10:40:20 +0200

    added missing prop for login route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,16 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Header from './components/Header';
-import NavbarMain from './components/NavbarMain';
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import NavbarMain from "./components/NavbarMain";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import Order from "./components/Order";
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [logged, setLogged] = useState(false);
@@ -24,13 +24,13 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login setName={setLogged} />} />
             <Route path="/order" element={<Order />} />
           </Routes>
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
